b5bf93a617b1c69c2e0469016b0e129340048d68
LucasFriedrich10 <lucasfriedrich10@hotmail.com>
2025-03-17 20:28:40 -0300

    Separando horários pelo período

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,8 @@
 import dayjs from "dayjs"
 import { openingHours } from "../../utils/opening-hours.js"
 
+const hours = document.getElementById("hours")
+
 export function hoursLoad({date}){
     const opening = openingHours.map((hour) => {
         const [scheduleHour] = hour.split(":")
@@ -15,4 +17,34 @@ export function hoursLoad({date}){
             available: isHourPast
         }
     })
+
+//renderizar os Horários
+    opening.forEach(({hour, available}) => {
+        const li = document.createElement("li")
+
+        li.classList.add("hour")
+        li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+        li.textContent = hour
+
+        if(hour === "9:00"){
+            hourHeaderAdd("Manhã")
+        }else if (hour === "13:00"){
+            hourHeaderAdd("Tarde")
+        }else if (hour === "18:00"){ 
+            hourHeaderAdd("Noite")
+        }
+
+        hours.append(li)
+    })
+
+}
+
+function hourHeaderAdd(title){
+    const header = document.createElement("li")
+
+    header.classList.add("hour-period")
+    header.textContent = title
+
+    hours.append(header)
 }
